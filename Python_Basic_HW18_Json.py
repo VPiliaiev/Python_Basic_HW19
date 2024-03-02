2937c777083e78a46b5f675c14e0ff63d122f676
Valerii Piliaiev <133023422+VPiliaiev@users.noreply.github.com>
2024-03-02 22:21:42 +0200

    Wrote dict in json file

---
+++ Python_Basic_HW18_Json.py
@@ -0,0 +1,14 @@
+import json
+
+input_data = {123456: ('Bob', 18),
+              654321: ('Tom', 20),
+              123654: ('Alex', 17),
+              321456: ('Sem', 22),
+              654456: ('Drake', 33),
+              125634: ('James', 34)}
+with open('text.json', 'w') as file:
+    json.dump(input_data, file)
+with open('text.json') as file:
+    data = json.load(file)
+    print(data)
+    
